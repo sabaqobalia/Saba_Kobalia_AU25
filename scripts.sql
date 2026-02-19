a24071343a95b8dcb9181b68829d2b4a22ad226f
sabaqobalia <74766639+sabaqobalia@users.noreply.github.com>
2026-02-19 22:37:22 +0400

    corrected task 5
    added cleansing layer and deduplicated data. made scripts rerunnable

--- scripts.sql
+++ scripts.sql
@@ -6,141 +6,306 @@ FOREIGN DATA WRAPPER file_fdw;
 
 
 --Create foreign tables for raw data:
-
-CREATE FOREIGN TABLE SA_SOURCE1.ext_source1 (
+CREATE FOREIGN TABLE IF NOT EXISTS SA_SOURCE1.ext_source1 (
     transaction_id        VARCHAR,
     store_name            VARCHAR,
     city                  VARCHAR,
-    transaction_date      TIMESTAMP,
+    transaction_date      VARCHAR,
     cashier_id            VARCHAR,
-    items_count           INTEGER,
-    total_amount_ngn      NUMERIC,
+    cashier_gender        VARCHAR,
+    cashier_birthday	  VARCHAR,
+    customer_id           VARCHAR,
+    customer_gender 	  VARCHAR,
+    customer_birthday     VARCHAR,
+    items_count           VARCHAR,
+    total_amount_ngn      VARCHAR,
     payment_method        VARCHAR,
-    discount_applied      BOOLEAN,
-    loyalty_points_earned INTEGER,
+    discount_applied      VARCHAR,
+    loyalty_points_earned VARCHAR,
     receipt_number        VARCHAR
 )
 SERVER csv_server
 OPTIONS (
-    filename 'C:/csv/source1.csv',
+    filename 'D:/sadesktopo/EPAM/stage 2/dwh/task 1/source1.csv',
     format 'csv',
     header 'true'
 );
 
 
-CREATE FOREIGN TABLE SA_SOURCE2.ext_source2 (
+CREATE FOREIGN TABLE IF NOT EXISTS SA_SOURCE2.ext_source2 (
     transaction_id        VARCHAR,
     store_name            VARCHAR,
     city                  VARCHAR,
-    transaction_date      TIMESTAMP,
+    transaction_date      VARCHAR,
     cashier_id            VARCHAR,
-    items_count           INTEGER,
-    total_amount_ngn      NUMERIC,
+    cashier_gender        VARCHAR,
+    cashier_birthday	  VARCHAR,
+    customer_id           VARCHAR,
+    customer_gender 	  VARCHAR,
+    customer_birthday     VARCHAR,
+    items_count           VARCHAR,
+    total_amount_ngn      VARCHAR,
     payment_method        VARCHAR,
-    discount_applied      BOOLEAN,
-    loyalty_points_earned INTEGER,
+    discount_applied      VARCHAR,
+    loyalty_points_earned VARCHAR,
     receipt_number        VARCHAR
 )
 SERVER csv_server
 OPTIONS (
-    filename 'C:/csv/source2.csv',
+    filename 'D:/sadesktopo/EPAM/stage 2/dwh/task 1/source2.csv',
     format 'csv',
     header 'true'
 );
+
 
 
 --create source tables
+CREATE TABLE IF NOT EXISTS sa_source1.src_source1
+( transaction_id          VARCHAR,
+    store_name            VARCHAR,
+    city                  VARCHAR,
+    transaction_date      VARCHAR,
+    cashier_id            VARCHAR,
+    cashier_gender        VARCHAR,
+    cashier_birthday	  VARCHAR,
+    items_count           VARCHAR,
+    customer_id           VARCHAR,
+    customer_gender 	  VARCHAR,
+    customer_birthday     VARCHAR,
+    total_amount_ngn      VARCHAR,
+    payment_method        VARCHAR,
+    discount_applied      VARCHAR,
+    loyalty_points_earned VARCHAR,
+    receipt_number        VARCHAR)
 
-CREATE TABLE SA_SOURCE1.src_source1 (
-    transaction_id        VARCHAR NOT NULL,
-    store_name            VARCHAR NOT NULL,
-    city                  VARCHAR NOT NULL,
-    transaction_date      TIMESTAMP NOT NULL,
-    cashier_id            VARCHAR NOT NULL,
-    items_count           INTEGER NOT NULL,
-    total_amount_ngn      NUMERIC(12,2) NOT NULL,
-    payment_method        VARCHAR NOT NULL,
-    discount_applied      BOOLEAN NOT NULL,
-    loyalty_points_earned INTEGER NOT NULL,
-    receipt_number        VARCHAR NOT NULL,
-    source_system         VARCHAR NOT NULL,
-    load_ts               TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
-CREATE TABLE SA_SOURCE2.src_source2 (
-    transaction_id        VARCHAR NOT NULL,
-    store_name            VARCHAR NOT NULL,
-    city                  VARCHAR NOT NULL,
-    transaction_date      TIMESTAMP NOT NULL,
-    cashier_id            VARCHAR NOT NULL,
-    items_count           INTEGER NOT NULL,
-    total_amount_ngn      NUMERIC(12,2) NOT NULL,
-    payment_method        VARCHAR NOT NULL,
-    discount_applied      BOOLEAN NOT NULL,
-    loyalty_points_earned INTEGER NOT NULL,
-    receipt_number        VARCHAR NOT NULL,
-    source_system         VARCHAR NOT NULL,
-    load_ts               TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS sa_source2.src_source2
+( transaction_id          VARCHAR,
+    store_name            VARCHAR,
+    city                  VARCHAR,
+    transaction_date      VARCHAR,
+    cashier_id            VARCHAR,
+    cashier_gender        VARCHAR,
+    cashier_birthday	  VARCHAR,
+    items_count           VARCHAR,
+    customer_id           VARCHAR,
+    customer_gender 	  VARCHAR,
+    customer_birthday     VARCHAR,
+    total_amount_ngn      VARCHAR,
+    payment_method        VARCHAR,
+    discount_applied      VARCHAR,
+    loyalty_points_earned VARCHAR,
+    receipt_number        VARCHAR)
 
 
 --insert into sources
 
 INSERT INTO SA_SOURCE1.src_source1 (
-    transaction_id,
-    store_name,
-    city,
-    transaction_date,
-    cashier_id,
-    items_count,
-    total_amount_ngn,
-    payment_method,
-    discount_applied,
-    loyalty_points_earned,
-    receipt_number,
-    source_system
+    transaction_id          ,
+    store_name            ,
+    city                  ,
+    transaction_date      ,
+    cashier_id            ,
+    cashier_gender        ,
+    cashier_birthday	  ,
+    items_count           ,
+    customer_id           ,
+    customer_gender 	  ,
+    customer_birthday     ,
+    total_amount_ngn      ,
+    payment_method        ,
+    discount_applied      ,
+    loyalty_points_earned ,
+    receipt_number
 )
 SELECT
-    transaction_id,
-    store_name,
-    city,
-    transaction_date,
-    cashier_id,
-    items_count,
-    total_amount_ngn,
-    payment_method,
-    discount_applied,
-    loyalty_points_earned,
-    receipt_number,
-    'SOURCE1_CSV'
+    transaction_id          ,
+    store_name            ,
+    city                  ,
+    transaction_date      ,
+    cashier_id            ,
+    cashier_gender        ,
+    cashier_birthday	  ,
+    items_count           ,
+    customer_id           ,
+    customer_gender 	  ,
+    customer_birthday     ,
+    total_amount_ngn      ,
+    payment_method        ,
+    discount_applied      ,
+    loyalty_points_earned ,
+    receipt_number
 FROM SA_SOURCE1.ext_source1;
 
+
 INSERT INTO SA_SOURCE2.src_source2 (
-    transaction_id,
-    store_name,
-    city,
-    transaction_date,
-    cashier_id,
-    items_count,
-    total_amount_ngn,
-    payment_method,
-    discount_applied,
-    loyalty_points_earned,
-    receipt_number,
-    source_system
+    transaction_id          ,
+    store_name            ,
+    city                  ,
+    transaction_date      ,
+    cashier_id            ,
+    cashier_gender        ,
+    cashier_birthday	  ,
+    items_count           ,
+    customer_id           ,
+    customer_gender 	  ,
+    customer_birthday     ,
+    total_amount_ngn      ,
+    payment_method        ,
+    discount_applied      ,
+    loyalty_points_earned ,
+    receipt_number
 )
 SELECT
-    transaction_id,
-    store_name,
-    city,
-    transaction_date,
-    cashier_id,
-    items_count,
-    total_amount_ngn,
-    payment_method,
-    discount_applied,
-    loyalty_points_earned,
-    receipt_number,
-    'SOURCE2_CSV'
+    transaction_id          ,
+    store_name            ,
+    city                  ,
+    transaction_date      ,
+    cashier_id            ,
+    cashier_gender        ,
+    cashier_birthday	  ,
+    items_count           ,
+    customer_id           ,
+    customer_gender 	  ,
+    customer_birthday     ,
+    total_amount_ngn      ,
+    payment_method        ,
+    discount_applied      ,
+    loyalty_points_earned ,
+    receipt_number
 FROM SA_SOURCE2.ext_source2;
 
+--create schema for cleansing layer
+CREATE SCHEMA IF NOT EXISTS BL_CL;
+
+-create mapping for cashiers, customers, cities and stores so that we dont insert duplicates in the 3NF layer.
+
+CREATE TABLE IF NOT EXISTS BL_CL.MAP_CASHIER (
+cashier_surr_id int,
+cashier_src_id varchar,
+cashier_gender varchar,
+cashier_birthday DATE,
+source_table varchar,
+source_system varchar
+);
+
+INSERT INTO BL_CL.MAP_CASHIER (cashier_src_id ,cashier_gender ,
+cashier_birthday ,source_table ,source_system )
+SELECT DISTINCT cashier_id, cashier_gender, cashier_birthday ::date, 'src_source1','sa_source1'
+FROM sa_source1.src_source1;
+
+INSERT INTO BL_CL.MAP_CASHIER (cashier_src_id ,cashier_gender ,
+cashier_birthday ,source_table ,source_system )
+SELECT DISTINCT cashier_id, cashier_gender, cashier_birthday ::date, 'src_source2','sa_source2'
+FROM sa_source2.src_source2;
+
+--now let's give them surrogate IDs
+
+WITH assign_id AS (
+SELECT cashier_src_id,
+	   DENSE_RANK () OVER (ORDER BY cashier_src_id) AS surr_id
+FROM bl_cl.map_cashier
+)
+UPDATE bl_cl.map_cashier c
+SET cashier_surr_id = a.surr_id
+FROM assign_id a
+WHERE c.cashier_src_id = a.cashier_src_id;
+
+
+CREATE TABLE IF NOT EXISTS bl_cl.map_customer(
+customer_surr_id int,
+customer_src_id varchar,
+customer_gender varchar,
+customer_birthday DATE,
+source_table varchar,
+source_system varchar
+);
+
+INSERT INTO BL_CL.MAP_Customer (customer_src_id ,customer_gender ,
+customer_birthday ,source_table ,source_system )
+SELECT DISTINCT customer_id, customer_gender, customer_birthday ::date, 'src_source1','sa_source1'
+FROM sa_source1.src_source1;
+
+INSERT INTO BL_CL.MAP_Customer (customer_src_id ,customer_gender ,
+customer_birthday ,source_table ,source_system )
+SELECT DISTINCT customer_id, customer_gender, customer_birthday ::date, 'src_source2','sa_source2'
+FROM sa_source2.src_source2;
+
+
+WITH assign_id AS (
+SELECT customer_src_id,
+DENSE_RANK () OVER (ORDER BY customer_src_id) AS surr_id
+FROM bl_cl.map_customer
+)
+UPDATE BL_CL.MAP_Customer c
+SET customer_surr_id = a.surr_id
+FROM assign_id a
+WHERE c.customer_src_id = a.customer_src_id;
+
+CREATE TABLE IF NOT EXISTS bl_cl.map_city (city_id int, city_name varchar,source_table varchar,
+source_system varchar);
+
+INSERT INTO bl_cl.map_city  (city_name ,source_table ,
+source_system )
+SELECT DISTINCT city, 'src_source1', 'sa_source1' FROM sa_source1.src_source1;
+
+INSERT INTO bl_cl.map_city  (city_name ,source_table ,
+source_system )
+SELECT DISTINCT city, 'src_source2', 'sa_source2' FROM sa_source2.src_source2;
+
+WITH assign_id AS (
+SELECT city_name,
+DENSE_RANK () OVER (ORDER BY city_name) AS surr_id
+FROM bl_cl.map_city
+)
+UPDATE bl_cl.map_city c
+SET city_id = a.surr_id
+FROM assign_id a
+WHERE c.city_name = a.city_name;
+
+
+CREATE TABLE IF NOT EXISTS bl_cl.store (
+store_id int, store_name varchar, store_city varchar, source_table varchar,
+source_system varchar
+);
+
+INSERT INTO bl_cl.store (store_name,store_city,source_table, source_system)
+SELECT DISTINCT store_name, city, 'src_source1','sa_source1'
+FROM sa_source1.src_source1;
+
+INSERT INTO bl_cl.store (store_name,store_city,source_table, source_system)
+SELECT DISTINCT store_name, city, 'src_source2','sa_source2'
+FROM sa_source2.src_source2;
+
+WITH assign_id AS (
+SELECT store_name, store_city,
+DENSE_RANK () OVER (ORDER BY store_name, store_city) AS surr_id
+FROM bl_cl.store
+)
+UPDATE bl_cl.store s
+SET store_id = surr_id 
+FROM assign_id a
+WHERE s.store_name = a.store_name
+AND s.store_city = a.store_city;
+
+CREATE TABLE IF NOT EXISTS bl_cl.payment_method (
+payment_method_id int, pm_name varchar, source_table varchar, source_system varchar
+);
+
+INSERT INTO  bl_cl.payment_method (pm_name,source_table,source_system)
+SELECT DISTINCT payment_method, 'src_source1', 'sa_source1' 
+FROM sa_source1.src_source1;
+
+INSERT INTO  bl_cl.payment_method (pm_name,source_table,source_system)
+SELECT DISTINCT payment_method, 'src_source2', 'sa_source2' 
+FROM sa_source2.src_source2
+
+WITH assign_id AS (
+SELECT pm_name,
+DENSE_RANK () OVER (ORDER BY pm_name) AS pm_id
+FROM  bl_cl.payment_method
+)
+UPDATE  bl_cl.payment_method p
+SET payment_method_id = a.pm_id
+FROM assign_id a
+WHERE p.pm_name = a.pm_name
